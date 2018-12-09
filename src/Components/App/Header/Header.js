a71b7358e45ad03b2a0521434750923f1e3e4cb2
Ashley Klein <ashklein6@gmail.com>
2018-12-09 16:06:33 -0600

    added gradient and animations to header

--- src/Components/App/Header/Header.js
+++ src/Components/App/Header/Header.js
@@ -1,21 +1,28 @@
 import React, { Component } from 'react';
 import { withStyles } from '@material-ui/core/styles';
+import './Header.css';
 import Typing from 'react-typing-animation';
+import Fade from 'react-reveal/Fade';
 
 class Header extends Component {
   render() {
+    const { classes } = this.props;
+
     return (
-        <div className="App-header">
-          <h1>
-            Ashley Klein
-          </h1>
-          <Typing loop speed={100} cursorClassName={styles.cursor}>
-          <h3>
-            Full-Stack Software Developer
-            <Typing.Delay ms={2000} />
-            <Typing.Backspace count={37} />
-          </h3>
-          </Typing>
+        <div className={classes.header}>
+          <Fade top delay={50} duration={2000}>
+            <h1>
+              Ashley Klein
+            </h1>
+          </Fade>
+          <div className={classes.subtitle}>
+            <Typing loop speed={100} count={29} cursorClassName={classes.cursor} startDelay={1500}>
+              <h3>
+                Full-Stack Software Developer
+                <Typing.Backspace count={28} delay={5000} />
+              </h3>
+            </Typing>
+          </div>
         </div>
     );
   }
@@ -23,10 +30,10 @@ class Header extends Component {
 
 const styles = {
   cursor: {
-    color: "white"
+    color: "white",
   },
   header: {
-    backgroundColor: "#282c34",
+    background: "linear-gradient(to bottom right, black, #1F3F3D)",
     minHeight: "100vh",
     display: "flex",
     flexDirection: "column",
@@ -34,6 +41,9 @@ const styles = {
     justifyContent: "center",
     fontSize: "calc(10px + 2vmin)",
     color: "white",
+  },
+  subtitle: {
+    height: "calc(10px + 2vmin)"
   }
 };
 
