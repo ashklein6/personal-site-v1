aeb1a47c737ebb66570b46519b7771f42d837a50
Ashley Klein <ashklein6@gmail.com>
2018-12-29 12:25:47 -0600

    completed implementation of react router

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,78 +1,22 @@
 import React, { Component } from 'react';
-import { HashRouter as Router, Route, Redirect } from 'react-router-dom';
-import ReactPageScroller from 'react-page-scroller';
-import { withStyles } from '@material-ui/core/styles';
-import Header from './Header/Header';
-import AboutMe from './AboutMe/AboutMe';
-import Experience from './Experience/Experience';
+import { HashRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+import PageScroller from './PageScroller/PageScroller';
+import Resume from './Resume/Resume';
 import './App.css';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      currentPage: 1
-    };
-    this._pageScroller = null;
-  }
-
-  pageOnChange = (number) => {
-    this.setState({ currentPage: number });
-    console.log('on page:', this.state.currentPage);
-  }
-
-  scrollDown = () => {
-    let newPage = this.state.currentPage;
-    this._pageScroller.goToPage(newPage);
-  }
-
-  scrollUp = () => {
-    let newPage = this.state.currentPage-2;
-    console.log('new page attempt:', newPage);
-    this._pageScroller.goToPage(newPage);
-  }
 
   render() {
-    const { classes } = this.props;
-
     return (
       <Router>
-        <Redirect exact from="/" to="/home" />
-        <Route exact path="/home" component={}/>
-        <React.Fragment>
-          <ReactPageScroller ref={c => this._pageScroller = c} pageOnChange={this.pageOnChange}>
-            <Experience />
-            <Header />
-            <AboutMe />
-          </ReactPageScroller>
-          {(this.state.currentPage > 1) ? 
-          <div className={classes.scrollUp} onClick={() => this.scrollUp()}>
-            &#x25B2;
-          </div> : null }
-          <div className={classes.scrollDown} onClick={() => this.scrollDown()}>
-            &#x25BC;
-          </div>
-        </React.Fragment>
+        <Switch>
+          <Redirect exact from="/" to="/home" />
+          <Route exact path="/home" component={PageScroller}/>
+          <Route exact path="/resume" component={Resume}/>
+        </Switch>
       </Router>
     );
   }
 }
 
-const styles = {
-  scrollDown: {
-    position: 'fixed',
-    bottom: '5%',
-    justifyContent: 'center',
-    left: '50%',
-    transform: 'translate(-50%, 0)',
-  },
-  scrollUp: {
-    position: 'fixed',
-    top: '5%',
-    justifyContent: 'center',
-    left: '50%',
-    transform: 'translate(-50%, 0)',
-  }
-};
-
-export default withStyles(styles)(App);
+export default App;
---
+++ src/Components/App/PageScroller/PageScroller.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import ReactPageScroller from 'react-page-scroller';
+import { withStyles } from '@material-ui/core/styles';
+import Header from '../Header/Header';
+import AboutMe from '../AboutMe/AboutMe'
+import Experience from '../Experience/Experience';
+
+class PageScroller extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentPage: 1
+    };
+    this._pageScroller = null;
+  }
+
+  pageOnChange = (number) => {
+    this.setState({ currentPage: number });
+    console.log('on page:', this.state.currentPage);
+  }
+
+  scrollDown = () => {
+    let newPage = this.state.currentPage;
+    this._pageScroller.goToPage(newPage);
+  }
+
+  scrollUp = () => {
+    let newPage = this.state.currentPage-2;
+    console.log('new page attempt:', newPage);
+    this._pageScroller.goToPage(newPage);
+  }
+
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <React.Fragment>
+        <ReactPageScroller ref={c => this._pageScroller = c} pageOnChange={this.pageOnChange}>
+          <Experience />
+          <Header />
+          <AboutMe />
+        </ReactPageScroller>
+        {(this.state.currentPage > 1) ? 
+        <div className={classes.scrollUp} onClick={() => this.scrollUp()}>
+          &#x25B2;
+        </div> : null }
+        <div className={classes.scrollDown} onClick={() => this.scrollDown()}>
+          &#x25BC;
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+const styles = {
+  scrollDown: {
+    position: 'fixed',
+    bottom: '5%',
+    justifyContent: 'center',
+    left: '50%',
+    transform: 'translate(-50%, 0)',
+  },
+  scrollUp: {
+    position: 'fixed',
+    top: '5%',
+    justifyContent: 'center',
+    left: '50%',
+    transform: 'translate(-50%, 0)',
+  }
+};
+
+export default withStyles(styles)(PageScroller);
